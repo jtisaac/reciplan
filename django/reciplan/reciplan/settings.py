bc411d0d6ab603a0898d55386c25613a2b239a19
jtisaac <jtisaac@users.noreply.github.com>
2021-03-03 23:05:52 -0600

    Added pathlib

--- django/reciplan/reciplan/settings.py
+++ django/reciplan/reciplan/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 import django_heroku
 import dj_database_url
+from pathlib import Path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
